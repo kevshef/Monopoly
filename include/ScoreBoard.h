b578afee467a605726f3a4bd58a1c2b0ce8c4904
Kevin Shefkiu <kevin.shefkiu@gmail.com>
2023-12-26 21:53:39 +0100

    add scoreboard

---
+++ include/ScoreBoard.h
@@ -0,0 +1,14 @@
+//
+// Created by Kevin Shefkiu on 26/12/23.
+//
+
+#ifndef MONOPOLY_SCOREBOARD_H
+#define MONOPOLY_SCOREBOARD_H
+
+
+class ScoreBoard {
+
+};
+
+
+#endif //MONOPOLY_SCOREBOARD_H
---
+++ src/ScoreBoard.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Kevin Shefkiu on 26/12/23.
+//
+
+#include "../include/ScoreBoard.h"
