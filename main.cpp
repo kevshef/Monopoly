71237e3770930c87cbf02534661cce639084226b
Kevin Shefkiu <kevin.shefkiu@gmail.com>
2024-01-07 18:49:52 +0100

    fix

--- main.cpp
+++ main.cpp
@@ -8,14 +8,14 @@ int main(int argc, char *argv[]) {
 
     const char *temp;
 
-    if(argc != 2) {
+    if (argc != 2) {
         std::cout << "Incorrect number of line arguments: must be 'human' or 'computer' \n";
         exit(1);
     }
-    if (argv[1] != nullptr && strcmp(argv[1], "computer")==0 || strcmp(argv[1], "COMPUTER") ==0 ) {
-        temp = static_cast<const char*>(argv[1]);
-    } else if(argv[1] != nullptr && strcmp(argv[1], "human")==0 || strcmp(argv[1], "HUMAN") ==0) {
-        temp = static_cast<const char*>(argv[1]);
+    if (argv[1] != nullptr && strcmp(argv[1], "computer") == 0 || strcmp(argv[1], "COMPUTER") == 0) {
+        temp = static_cast<const char *>(argv[1]);
+    } else if (argv[1] != nullptr && strcmp(argv[1], "human") == 0 || strcmp(argv[1], "HUMAN") == 0) {
+        temp = static_cast<const char *>(argv[1]);
     } else {
         std::cout << "invalid command line argument: must be 'human' or 'computer' \n";
         exit(1);
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
 
     std::cout << board;
 
-    Game gioco("computer", board);
+    Game gioco(temp, board);
 
     std::cout << gioco;
 
@@ -37,8 +37,8 @@ int main(int argc, char *argv[]) {
 
         std::cout << "\n\n turno " << (turno + 1) << " : \n";
 
-        for(int i = 0; i < 4; i++) {
-            if(!gioco.getPlayers()[i]->isBankrupt()) {
+        for (int i = 0; i < 4; i++) {
+            if (!gioco.getPlayers()[i]->isBankrupt()) {
                 int moveResult = gioco.move(board, i);
             }
 
@@ -46,10 +46,11 @@ int main(int argc, char *argv[]) {
 
         turno++;
 
-    } while(!gioco.end() && turno < 5000);
+    } while (!gioco.end() && turno < 5000);
 
     int id = gioco.richestPlayer();
-    std::cout << "Il vincitore è il giocatore " << gioco.getPlayers()[id]->getNumber() << " con un saldo di: " << gioco.getPlayers()[id]->getBalance();
+    std::cout << "Il vincitore è il giocatore " << gioco.getPlayers()[id]->getNumber() << " con un saldo di: "
+              << gioco.getPlayers()[id]->getBalance() << " fiorini";
 
     return 0;
 }
