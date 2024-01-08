09e54d3f19f1eae03aecdb0f1ca645b591cb184a
ViseGarbe <151759712+ViseGarbe@users.noreply.github.com>
2024-01-08 21:37:14 +0100

    Function move from int to void #2

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ int main(int argc, char *argv[]) {
 
         for (int i = 0; i < 4; i++) {
             if (!gioco.getPlayers()[i]->isBankrupt()) {
-                int moveResult = gioco.move(board, i);
+                gioco.move(board, i);
             }
         }
 
